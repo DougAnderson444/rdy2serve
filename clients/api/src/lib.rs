15c23aa489df9582d2288f36106096ed754d3168
Doug A <douganderson444@gmail.com>
2023-03-04 08:29:03 -0400

    add client api

---
+++ clients/api/src/lib.rs
@@ -0,0 +1,13 @@
+use libp2p::multiaddr::Multiaddr;
+
+pub async fn connect(text: String) {
+    todo!();
+    // if let Ok(address) = self.text.parse::<Multiaddr>() {
+    //     self.send_command(Command::Dial(address));
+    // } else {
+    //     self.messages.push_back((
+    //         Color32::RED,
+    //         format!("Invalid multiaddr {}", self.text.clone()),
+    //     ));
+    // }
+}

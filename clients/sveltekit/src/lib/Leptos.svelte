ca59078cbcb1bf813c82f449caeddb7e50df9a98
Doug A <douganderson444@gmail.com>
2023-03-06 12:34:17 -0400

    segregate leptos, add tailwindcss

---
+++ clients/sveltekit/src/lib/Leptos.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { onMount } from "svelte"
+
+    // When using `cargo leptos --watch` (does not work for front end)
+    // import init from "../../../../target/front/wasm32-unknown-unknown/debug/leptos_start.wasm?init"
+
+    // When using `trunk serve --open --features=hydrate` (works)
+    import init from "../../../leptos/dist/leptos_start.js"
+
+    onMount(async () => {
+        await init()
+    })
+
+    </script>
